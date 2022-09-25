f0b33cb39b8d7e7d702248513667803bf30eeb6b
am <am2022a2@gmail.com>
2022-09-25 07:36:22 +0330

    add functionality to open item

--- notepad/notepad/mainwindow.cpp
+++ notepad/notepad/mainwindow.cpp
@@ -30,5 +30,26 @@ void MainWindow::on_actionNew_triggered()
 
 void MainWindow::on_actionOpen_triggered()
 {
+    QMessageBox msg;
+    QString filename = QFileDialog::getOpenFileName(this, "Add File", "/home", tr("Text Files (*.txt)"));
 
+    QFile file(filename);
+
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        msg.setText("you enter invalid file!");
+        msg.exec();
+        file.close();
+        return;
+    }
+
+    this->setWindowTitle(filename);
+
+    QTextStream read(&file);
+
+    QString text = read.readAll();
+
+    ui->textEdit->setText(text);
+
+    file.close();
 }
--- notepad/notepad/mainwindow.h
+++ notepad/notepad/mainwindow.h
@@ -20,6 +20,8 @@ private slots:
 
     void on_actionNew_triggered();
 
+    void on_actionOpen_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
