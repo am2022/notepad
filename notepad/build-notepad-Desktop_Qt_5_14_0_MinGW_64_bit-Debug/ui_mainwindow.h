557a2d0796c93cc73bf7bf408ecbda9a77554f7a
am <am2022a2@gmail.com>
2022-10-27 09:51:26 +0330

    build app with new added shortcut

--- notepad/build-notepad-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ notepad/build-notepad-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -212,6 +212,9 @@ public:
         actionNew_Window->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Shift+N", nullptr));
 #endif // QT_CONFIG(shortcut)
         actionExit_All->setText(QCoreApplication::translate("MainWindow", "Exit All", nullptr));
+#if QT_CONFIG(shortcut)
+        actionExit_All->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Shift+Q", nullptr));
+#endif // QT_CONFIG(shortcut)
         menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
         menuEdit->setTitle(QCoreApplication::translate("MainWindow", "Edit", nullptr));
         menuView->setTitle(QCoreApplication::translate("MainWindow", "View", nullptr));
