9f88d464373b0c23801b04974ddccd306ebd906e
am <am2022a2@gmail.com>
2022-11-24 10:36:38 +0330

    change color of pushbutton and label and spinbox

--- notepad/notepad/font_frm.cpp
+++ notepad/notepad/font_frm.cpp
@@ -6,6 +6,10 @@ font_frm::font_frm(QWidget *parent) :
     ui(new Ui::font_frm)
 {
     ui->setupUi(this);
+    setStyleSheet("background-color:white;");
+    ui->pushButton->setStyleSheet("background-color:rgb(122, 235, 52);");
+    ui->label->setStyleSheet("background-color:white;");
+    ui->spinBox->setStyleSheet("background-color:white;");
 }
 
 font_frm::~font_frm()
